46b867bba8f5e40ad1dea29893db54e3d37d7c27
horv-fannieu <VerdinVillicana8040@outlook.com>
2025-12-05 06:44:26 +0800

    add decorator-advanced-responsive.java, secure.txt, lib.ts

---
+++ email-download/mock-gateway-backend/decorator-advanced-responsive.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\email-download\mock-gateway-backend\decorator-advanced-responsive.java
+// Generated at: 2025-10-20T16:15:50.523Z
---
+++ email-download/mock-gateway-backend/resolver-api-helpers/lib.ts
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+interface complexProps { data: string }
